03e343e9e1981bdb30d347f25e35dd4ef1e9af27
Yordan Yosef Ramirez Calzado <yramirec@everis.com>
2019-09-17 14:19:37 -0500

    Swagger WebFlux

--- src/main/java/com/everis/MicroserviceStudentApplication.java
+++ src/main/java/com/everis/MicroserviceStudentApplication.java
@@ -3,6 +3,9 @@ package com.everis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+
+@EnableSwagger2WebFlux
 @SpringBootApplication
 public class MicroserviceStudentApplication {
 
